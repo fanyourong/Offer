f20735d60c0b256f80d84d6d5b75e4b8542036c7
fanyourong <280767855@qq.com>
2020-02-24 15:07:14 +0800

    小青蛙跳台阶，112358

---
+++ jumpFloor/jumpFloor/jumpFloor.cpp
@@ -0,0 +1,26 @@
+﻿// jumpFloor.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+int jumpFloor(int number) 
+{
+	if (number == 1)
+		return 1;
+	if (number == 2)
+		return 2;
+	int res = 0;
+	int one = 1, two = 2;
+	for (int i = 3; i <= number; i++)
+	{
+		res = one + two;
+		one = two;
+		two = res;
+	}
+	return res;
+}
