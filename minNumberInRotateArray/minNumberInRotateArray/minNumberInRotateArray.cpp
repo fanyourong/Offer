2053d07ef22fcfb68da4f7122e810a8c7f62bdef
fanyourong <280767855@qq.com>
2020-02-24 14:29:38 +0800

    暴力查找

---
+++ minNumberInRotateArray/minNumberInRotateArray/minNumberInRotateArray.cpp
@@ -0,0 +1,27 @@
+﻿// minNumberInRotateArray.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+
+	int minNumberInRotateArray(vector<int> rotateArray) 
+	{
+		if (rotateArray.size() == 0)
+			return 0;
+		int min = rotateArray[0];
+		for (int i = 0; i < rotateArray.size(); i++)
+		{
+			if (min > rotateArray[i])
+				min = rotateArray[i];
+		}
+		return min;
+	}
+};
