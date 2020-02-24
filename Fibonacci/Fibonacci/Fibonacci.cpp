8959e25743bccd1e040b0a4d0387c6b70c68bd28
fanyourong <280767855@qq.com>
2020-02-24 15:07:46 +0800

    Fibonacci数列

---
+++ Fibonacci/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,24 @@
+﻿// Fibonacci.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+int Fibonacci(int n) 
+{
+	if (n == 0)
+		return 0;
+	if (n == 1 || n == 2)
+		return 1;
+	int a = 1, b = 1, result = 0;
+	for (int i = 3; i <= n; i++)
+	{
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	return result;
+}
