fb6249c4247929584fe0f503856b49c272847ced
fanyourong <280767855@qq.com>
2020-02-25 18:11:49 +0800

    一个二进制的数它有多少个一，n=n&(n-1)

---
+++ NumberOf1/NumberOf1/NumberOf1.cpp
@@ -0,0 +1,22 @@
+﻿// NumberOf1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	int  NumberOf1(int n) {
+		int count = 0;
+		while (n != 0)
+		{
+			n = n & (n - 1);
+			count++;
+		}
+		return count;
+	}
+};
