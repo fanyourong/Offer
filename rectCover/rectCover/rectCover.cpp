7e282a421c01a78d16012d8a5f97ff52ad4a605a
fanyourong <280767855@qq.com>
2020-02-25 18:13:24 +0800

    摆放小矩形，还是递归

---
+++ rectCover/rectCover/rectCover.cpp
@@ -0,0 +1,25 @@
+﻿// rectCover.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	int rectCover(int number) {
+		if (number < 1)
+			return 0;
+		else if (number == 1)
+			return 1;
+		else if (number == 2)
+			return 2;
+		else
+		{
+			return rectCover(number - 1) + rectCover(number - 2);
+		}
+	}
+};
