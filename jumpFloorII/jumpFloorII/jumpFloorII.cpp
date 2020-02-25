43a9e1b4fac61118e17ce99bd2857b94bc168878
fanyourong <280767855@qq.com>
2020-02-25 18:10:35 +0800

    小青蛙可以随便跳f(n)=2*f(n-1)

---
+++ jumpFloorII/jumpFloorII/jumpFloorII.cpp
@@ -0,0 +1,24 @@
+﻿// jumpFloorII.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	int jumpFloorII(int number) 
+	{
+		if (number <= 0)
+			return -1;
+		else if (number == 1)
+			return 1;
+		else
+		{
+			return 2 * jumpFloorII(number - 1);
+		}
+	}
+};
