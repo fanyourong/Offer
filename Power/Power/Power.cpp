9f6dea0904e846675b47d9a615d1806f14aaf9b3
fanyourong <280767855@qq.com>
2020-02-28 19:28:04 +0800

    指数&1，符合就乘到结果上；base*base,指数右移；base>0?res:1/res

---
+++ Power/Power/Power.cpp
@@ -0,0 +1,36 @@
+﻿// Power.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	double Power(double base, int exponent) {
+		double res = 1;
+		double temp = base;
+		if (base == 0 && exponent < 0)
+			return false;
+		else if (base == 0 && exponent == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			while (exponent != 0)
+			{
+				if ((exponent & 1) == 1)
+				{
+					res = res * temp;
+				}
+				temp = temp * temp;
+				exponent >> 1;
+			}
+			return base >= 0 ? res : 1 / res;
+		}
+	}
+};
