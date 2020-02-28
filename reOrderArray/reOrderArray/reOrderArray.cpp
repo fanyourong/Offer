4341f92ba26032cb27e408f44d0411dc83026c92
fanyourong <280767855@qq.com>
2020-02-28 19:28:47 +0800

    奇偶排序

---
+++ reOrderArray/reOrderArray/reOrderArray.cpp
@@ -0,0 +1,28 @@
+﻿// reOrderArray.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+class Solution {
+public:
+	void reOrderArray(vector<int> &array) {
+		vector<int> res;
+		int length = array.size();
+		for (int i = 0; i < length; i++)
+		{
+			if (array[i] % 2 == 1)
+				res.push_back(array[i]);
+		}
+		for (int i = 0; i < length; i++)
+		{
+			if (array[i] % 2 == 0)
+				res.push_back(array[i]);
+		}
+		array = res;
+	}
+};
