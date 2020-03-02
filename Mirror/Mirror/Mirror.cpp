175cabc03a5eb14e63d9ac9132d33d95ac72fb19
fanyourong <280767855@qq.com>
2020-03-02 16:55:01 +0800

    操作给定的二叉树，将其变换为源二叉树的镜像。

---
+++ Mirror/Mirror/Mirror.cpp
@@ -0,0 +1,33 @@
+﻿// Mirror.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x):
+		val(x),left(nullptr),right(nullptr){}
+};
+
+class Solution {
+public:
+	void Mirror(TreeNode *pRoot) 
+	{
+		if (pRoot == nullptr)
+			return;
+		TreeNode* temp = nullptr;
+		temp = pRoot->left;
+		pRoot->left = pRoot->right;
+		pRoot->right = temp;
+		Mirror(pRoot->left);
+		Mirror(pRoot->right);
+	}
+};
