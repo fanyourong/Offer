4e8af199ece8d316c01c1338b91bc1896d9c522b
fanyourong <280767855@qq.com>
2020-03-06 17:10:25 +0800

    判断是否可能为出栈顺序

---
+++ IsPopOrder/IsPopOrder/IsPopOrder.cpp
@@ -0,0 +1,28 @@
+﻿// IsPopOrder.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	bool IsPopOrder(vector<int> pushV, vector<int> popV) 
+	{
+		vector<int> temp;
+		for (int i = 0,j = 0; i < pushV.size();)
+		{
+			temp.push_back(pushV[i++]);
+			while (j<popV.size() && popV[j] == temp.back())
+			{
+				temp.pop_back();
+				j++;
+			}
+		}
+		return temp.empty();
+	}
+};
