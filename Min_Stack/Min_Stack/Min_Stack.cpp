4d4ed11684d0974f3d6c1a6d87372fbe10178097
fanyourong <280767855@qq.com>
2020-03-06 16:28:57 +0800

    定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数

---
+++ Min_Stack/Min_Stack/Min_Stack.cpp
@@ -0,0 +1,40 @@
+﻿// Min_Stack.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stack> 
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution {
+public:
+	stack<int> stack1;
+	stack<int> stack_min;
+	void push(int value) {
+		stack1.push(value);
+		if (stack_min.empty())
+		{
+			stack_min.push(value);
+		}
+		if(value<=stack_min.top())
+		{
+			stack_min.push(value);
+		}
+	}
+	void pop() {
+		if (stack1.top() == stack_min.top())
+		{
+			stack_min.pop();
+		}
+		stack1.pop();
+	}
+	int top() {
+		return stack1.top();
+	}
+	int min() {
+		return stack_min.top();
+	}
+};
