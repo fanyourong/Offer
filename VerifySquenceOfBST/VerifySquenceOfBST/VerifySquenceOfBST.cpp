26eeb6b7512249c76adc7806c7c3f18e831d7ed2
fanyourong <280767855@qq.com>
2020-03-07 21:26:40 +0800

    BST,左<根<右、

---
+++ VerifySquenceOfBST/VerifySquenceOfBST/VerifySquenceOfBST.cpp
@@ -0,0 +1,47 @@
+﻿// VerifySquenceOfBST.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+class Solution 
+{
+public:
+	bool VerifySquenceOfBST(vector<int> sequence) 
+	{
+		return BST(sequence, 0, sequence.size() - 1);
+	}
+
+	bool BST(vector<int> vec, int begin, int end)
+	{
+		if (begin > end || vec.empty())
+			return false;
+		int root = vec[end];
+		int i = begin;
+		for (; i < end; i++)
+		{
+			if (vec[i] > root)
+			{
+				break;
+			}
+		}
+		for (int j = i; j < end; j++)
+		{
+			if (root > vec[j])
+				return false;
+		}
+		bool left = true;
+		if (i > begin)
+			left = BST(vec, begin, i-1);
+		bool right = true;
+		if (i < end - 1)
+			right = BST(vec, i, end - 1);
+		return left && right;
+	
+	}
+};
