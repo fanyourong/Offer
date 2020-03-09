442598d9994f7d486b6ba6c4fb7651b216ebec76
fanyourong <280767855@qq.com>
2020-03-09 18:49:04 +0800

    在二叉树中，找到和为某值的若干路径，#又是递归#

---
+++ FindPath/FindPath/FindPath.cpp
@@ -0,0 +1,50 @@
+﻿// FindPath.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	vector<vector<int> > res;
+	vector <int> temp;
+	void Find(TreeNode* node, int expectNumber) 
+	{
+		int now = node->val;
+		if (node == nullptr)
+			return;
+		temp.push_back(node->val);
+		if (expectNumber-now == 0&& node->left == nullptr&&node->right==nullptr)
+		{
+			res.push_back(temp);
+		}
+		else
+		{
+			if (node->left != nullptr)
+				Find(node->left, expectNumber - now);
+			if (node->right != nullptr)
+				Find(node->right, expectNumber - now);
+		}
+		temp.pop_back();
+	}
+
+	vector<vector<int> > FindPath(TreeNode* root, int expectNumber)
+	{
+		Find(root, expectNumber);
+		return res;
+	}
+};
